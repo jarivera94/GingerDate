eebe59cca7dbd3760446235e113da45911792d45
Jael Alexander Rivera Oviedo <jarivera94@hotmail.com>
2019-09-13 14:47:19 -0500

    Add monthAcronym

--- src/app/core/services/ginger-date-month.service.ts
+++ src/app/core/services/ginger-date-month.service.ts
@@ -8,18 +8,18 @@ export class GingerDateMonthService {
   private months: any[];
   constructor() {
     this.months = new Array();
-    this.months.push({'monthKey': '01', 'monthName': 'Enero'});
-    this.months.push({'monthKey': '02', 'monthName': 'Febrero'});
-    this.months.push({'monthKey': '03', 'monthName': 'Marzo'});
-    this.months.push({'monthKey': '04', 'monthName': 'Abril'});
-    this.months.push({'monthKey': '05', 'monthName': 'Mayo'});
-    this.months.push({'monthKey': '06', 'monthName': 'Junio'});
-    this.months.push({'monthKey': '07', 'monthName': 'Julio'});
-    this.months.push({'monthKey': '08', 'monthName': 'Agosto'});
-    this.months.push({'monthKey': '09', 'monthName': 'Septiembre'});
-    this.months.push({'monthKey': '10', 'monthName': 'Octubre'});
-    this.months.push({'monthKey': '11', 'monthName': 'Noviembre'});
-    this.months.push({'monthKey': '12', 'monthName': 'Diciembre'});
+    this.months.push({'monthKey': '01', 'monthName': 'Enero' , 'monthKeyAcronym': 'Ene'});
+    this.months.push({'monthKey': '02', 'monthName': 'Febrero', 'monthKeyAcronym': 'Feb'});
+    this.months.push({'monthKey': '03', 'monthName': 'Marzo', 'monthKeyAcronym': 'Mar'});
+    this.months.push({'monthKey': '04', 'monthName': 'Abril', 'monthKeyAcronym': 'Abr'});
+    this.months.push({'monthKey': '05', 'monthName': 'Mayo', 'monthKeyAcronym': 'May'});
+    this.months.push({'monthKey': '06', 'monthName': 'Junio', 'monthKeyAcronym': 'Jun'});
+    this.months.push({'monthKey': '07', 'monthName': 'Julio', 'monthKeyAcronym': 'Jul'});
+    this.months.push({'monthKey': '08', 'monthName': 'Agosto', 'monthKeyAcronym': 'Ago'});
+    this.months.push({'monthKey': '09', 'monthName': 'Septiembre', 'monthKeyAcronym': 'Sep'});
+    this.months.push({'monthKey': '10', 'monthName': 'Octubre', 'monthKeyAcronym': 'Oct'});
+    this.months.push({'monthKey': '11', 'monthName': 'Noviembre', 'monthKeyAcronym': 'Nov'});
+    this.months.push({'monthKey': '12', 'monthName': 'Diciembre', 'monthKeyAcronym': 'Dic'});
   }
   public getMonths(): any[] {
     return this.months;
